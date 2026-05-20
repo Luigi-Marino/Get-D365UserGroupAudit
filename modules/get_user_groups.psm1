2efb82fc86d8ecf36e2020511eb2e545247cf95f
Luigi Marino <emails@luigi-marino.com>
2026-05-20 15:25:17 +0100

    Separate out environments

--- modules/get_user_groups.psm1
+++ modules/get_user_groups.psm1
@@ -1,17 +1,55 @@
 function Get-UserGroups {
     $prefix = "AA - LEGO"
     $users = Get-ADUser -Filter * -Properties MemberOf, DisplayName, UserPrincipalName, Title
+
     $results = foreach ($user in $users) {
-        $groupNames = $user.MemberOf | ForEach-Object { ($_ -split ",")[0] -replace "^CN=", "" }
+
+        # Extract group names cleanly
+        $groupNames = $user.MemberOf | ForEach-Object {
+            ($_ -split ",")[0] -replace "^CN=", ""
+        }
+
+        # Filter only LEGO groups
         $rbacGroups = $groupNames | Where-Object { $_ -like "$prefix*" }
 
         if ($rbacGroups.Count -gt 0) {
+
+            # --- NON-PROD groups ---
+            $nonProdTokens = @("NON-PROD", "ACC", "TST", "PRF")
+            $nonProdGroups = $rbacGroups | Where-Object {
+                $grp = $_
+                $nonProdTokens | ForEach-Object { $grp -like "*$_*" }
+            }
+
+            # --- PowerBI groups ---
+            $powerBiGroups = $rbacGroups | Where-Object { $_ -like "*ADL*" }
+
+            # --- PROD groups (everything else) ---
+            $prodGroups = $rbacGroups |
+                Where-Object {
+                    $_ -notin $nonProdGroups -and
+                    $_ -notin $powerBiGroups
+                }
+
             [pscustomobject]@{
-                Username    = $user.SamAccountName
-                DisplayName = $user.DisplayName
-                UPN         = $user.UserPrincipalName
-                JobTitle    = $user.Title
-                RBACGroups  = [string]::Join(", ", $rbacGroups)
+                Username      = $user.SamAccountName
+                DisplayName   = $user.DisplayName
+                UPN           = $user.UserPrincipalName
+                JobTitle      = $user.Title
+
+                # Counts
+                TotalGroupCount = $rbacGroups.Count
+                ProdCount       = $prodGroups.Count
+                NonProdCount    = $nonProdGroups.Count
+                PowerBiCount    = $powerBiGroups.Count
+
+                # Group lists
+                ProdGroups      = [string]::Join(", ", $prodGroups)
+                NonProdGroups   = [string]::Join(", ", $nonProdGroups)
+                PowerBiGroups   = [string]::Join(", ", $powerBiGroups)
+
+                # Raw list if needed
+                AllGroups       = [string]::Join(", ", $rbacGroups)
             }
         }
     }
