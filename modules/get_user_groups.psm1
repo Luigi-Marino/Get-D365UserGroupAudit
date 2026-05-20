6d261b05988a6a2e25d9134cda4c68881801dc14
Luigi Marino <emails@luigi-marino.com>
2026-05-20 15:35:40 +0100

    Fixed issue with single items

--- modules/get_user_groups.psm1
+++ modules/get_user_groups.psm1
@@ -4,51 +4,51 @@ function Get-UserGroups {
 
     $results = foreach ($user in $users) {
 
-        # Extract group names cleanly
         $groupNames = $user.MemberOf | ForEach-Object {
             ($_ -split ",")[0] -replace "^CN=", ""
         }
 
-        # Filter only LEGO groups
-        $rbacGroups = $groupNames | Where-Object { $_ -like "$prefix*" }
+        # FORCE ARRAY
+        $rbacGroups = @($groupNames | Where-Object { $_ -like "$prefix*" })
 
         if ($rbacGroups.Count -gt 0) {
 
-            # --- NON-PROD groups ---
             $nonProdTokens = @("NON-PROD", "ACC", "TST", "PRF")
-            $nonProdGroups = $rbacGroups | Where-Object {
-                $grp = $_
-                $nonProdTokens | ForEach-Object { $grp -like "*$_*" }
-            }
 
-            # --- PowerBI groups ---
-            $powerBiGroups = $rbacGroups | Where-Object { $_ -like "*ADL*" }
-
-            # --- PROD groups (everything else) ---
-            $prodGroups = $rbacGroups |
-                Where-Object {
-                    $_ -notin $nonProdGroups -and
-                    $_ -notin $powerBiGroups
+            # FORCE ARRAYS EVERYWHERE
+            $nonProdGroups = @(
+                $rbacGroups | Where-Object {
+                    $grp = $_
+                    $nonProdTokens | ForEach-Object { $grp -like "*$_*" }
                 }
+            )
+
+            $powerBiGroups = @(
+                $rbacGroups | Where-Object { $_ -like "*ADL*" }
+            )
+
+            $prodGroups = @(
+                $rbacGroups |
+                    Where-Object {
+                        $_ -notin $nonProdGroups -and
+                        $_ -notin $powerBiGroups
+                    }
+            )
 
             [pscustomobject]@{
-                Username      = $user.SamAccountName
-                DisplayName   = $user.DisplayName
-                UPN           = $user.UserPrincipalName
-                JobTitle      = $user.Title
+                Username        = $user.SamAccountName
+                DisplayName     = $user.DisplayName
+                UPN             = $user.UserPrincipalName
+                JobTitle        = $user.Title
 
-                # Counts
                 TotalGroupCount = $rbacGroups.Count
                 ProdCount       = $prodGroups.Count
                 NonProdCount    = $nonProdGroups.Count
                 PowerBiCount    = $powerBiGroups.Count
 
-                # Group lists
                 ProdGroups      = [string]::Join(", ", $prodGroups)
                 NonProdGroups   = [string]::Join(", ", $nonProdGroups)
                 PowerBiGroups   = [string]::Join(", ", $powerBiGroups)
-
-                # Raw list if needed
                 AllGroups       = [string]::Join(", ", $rbacGroups)
             }
         }
@@ -56,5 +56,3 @@ function Get-UserGroups {
 
     return $results
 }
-
-Export-ModuleMember -Function Get-UserGroups
